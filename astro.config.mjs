5631ef0e2775f19b09e40e4c912fac507d3a2eac
Abdulrehman <abdulrehmantehseen123f@gmail.com>
2026-01-29 15:53:30 +0500

    added final animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [icon(), sitemap()],
+  site: "https://trip2go.co.uk/"
 });
